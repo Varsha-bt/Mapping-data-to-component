9376504900ada93444bbcf2613d75b8e35bda203
Varsha-bt <78480432+Varsha-bt@users.noreply.github.com>
2021-03-04 10:03:45 +0530

    Initial commit

---
+++ src/cards.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./deatails";
+function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <p>{props.id}</p>
+          <h2 className="name">{props.name}</h2>
+          <Avatar img={props.img} />
+        </div>
+        <div className="bottom">
+          <Detail detailInfo={props.phno} />
+          <Detail detailInfo={props.email} />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
---
+++ src/contacts.jsx
@@ -0,0 +1,27 @@
+const Contact = [
+  {
+    id:1,
+    name:"Annie",
+    image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    tel:"+91273746568",
+    mail:"Annie@gmail.com"
+  },
+  {
+      id:2,
+      name:"jenny",
+      image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      tel:"+91273746568",
+      mail:"jenny@gmail.com"
+  },
+  {
+      id:3,
+      name:"shiny",
+      image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      tel:"+91273746568",
+      mail:"shiny@gmail.com"
+
+  }
+
+];
+
+export default Contact;
